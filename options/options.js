d916c6661bdef65b479f96aa69e36c8b461f8141
Crowdbotics <team@crowdbotics.com>
2024-01-26 19:04:53 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kaiser-patient-mobi-45582",
+  url: "https://kaiser-patient-mobi-45582.botics.co",
+  api: "https://kaiser-patient-mobi-45582.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
